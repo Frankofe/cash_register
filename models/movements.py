0f8b78feef4c43247a4ec457a039546a59c7b8c9
Franco Fernandez <frankofe1997@gmail.com>
2022-05-29 03:31:35 -0300

    Filters and negative balance

--- models/movements.py
+++ models/movements.py
@@ -1,25 +1,16 @@
-from email.policy import default
-from time import strftime
 from odoo import models, fields, api
-from datetime import *
-# from odoo.exceptions import *
-import logging
-logger = logging.getLogger(__name__)
-
-
+from odoo.exceptions import ValidationError
 
 class movements(models.Model):
     _name = 'cash_register.movements'
-    _descripcion='Register in a history the movements in the cash register'
-    _order='create_date desc'
-    
+    _descripcion='Register in a history the movements in the cash register'    
 
     type= fields.Selection(string='Movement type', selection=[('i','Entry'),('e','Egress')],required=True)
     amount=fields.Float(string='Amount', required=True)
     balance=fields.Float(string='Balance',help='Current balance', compute='_balance', required=True)
     contact=fields.Many2one('res.partner', required=True)
 
-    @api.onchange('balance','amount','type', 'id')
+    @api.depends('balance','amount','type')
     def _balance(self):
         for record in self:
           balance = 0
@@ -30,3 +21,10 @@ class movements(models.Model):
                 record.balance = record.balance+movement.amount
             else:
                 record.balance = record.balance-movement.amount
+    
+    @api.constrains('amount')
+    def _constrains_amount(self):
+      domain = [('id','!=', self.id)]
+      record=self.search(domain, order='id desc', limit=1)
+      if record.balance < self.amount and self.type == 'e':
+        raise ValidationError("Negative balance is not allowed.")
